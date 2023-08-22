860d996d9a18ea8fccf9cbee08b4b9bb304d81b8
abrilxcx <abrlbjrn@gmail.com>
2023-08-21 23:33:17 -0600

    contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hello World';
   public counter: number = 10;
+
+  increaseBy( value: number ): void {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
+
 }
 
