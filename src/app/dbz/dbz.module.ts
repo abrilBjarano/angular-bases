351435c471e511be1e7b114c7637f55c14a25c4b
abrilxcx <abrlbjrn@gmail.com>
2023-08-24 12:53:42 -0600

    modulo dbz

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MainPageComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  exports: [
+    MainPageComponent
+  ],
+  declarations: [
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
