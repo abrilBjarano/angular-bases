a204841449d9f37cbe14330e1aa005768a179a2e
abrilxcx <abrlbjrn@gmail.com>
2023-08-26 18:45:55 -0600

    formas de depurar la app

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -17,7 +17,6 @@ export class AddCharacterComponent {
   }
 
   emitCharacter(): void {
-    console.log(this.character);
 
     if( this.character.name.length === 0) {
       alert('No empty character is valid!');
@@ -26,8 +25,7 @@ export class AddCharacterComponent {
 
     this.onNewCharacter.emit(this.character);
 
-    this.character.name = '';
-    this.character.power = 0;
+    this.character = { name: '', power: 0 };
   }
 
 }
